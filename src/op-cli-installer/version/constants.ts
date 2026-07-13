4135e6c81a2b88beb0365ca424713dfdfee07a0f
Jill Regan <jill.regan@agilebits.com>
2026-07-13 17:24:48 -0400

    Fallback to docker hub for cli install

--- src/op-cli-installer/version/constants.ts
+++ src/op-cli-installer/version/constants.ts
@@ -3,11 +3,16 @@ export enum ReleaseChannel {
 	latestBeta = "latest-beta",
 }
 
+// Fields are optional since this is untrusted JSON parsed from a remote response
+// that may be unreachable or return an unexpected shape.
 export interface VersionResponse {
-	// eslint disabled next line as CLI2 is expected in getting CLI versions response
-	/* eslint-disable-next-line @typescript-eslint/naming-convention */
-	CLI2: {
-		release: { version: string };
-		beta: { version: string };
+	CLI2?: {
+		release?: { version?: string };
+		beta?: { version?: string };
 	};
+}
+
+// As above, results is optional since the Docker Hub response is untrusted JSON.
+export interface DockerHubTagsResponse {
+	results?: { name: string }[];
 }
